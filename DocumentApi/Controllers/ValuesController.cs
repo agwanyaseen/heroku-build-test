57f3035a8295d7604f2465e665417cbad44dd43c
Yaseen Agwan <yaseen.agwan@nationalautocare.com>
2020-10-08 22:13:18 +0530

    Added new api project and removed old one

---
+++ DocumentApi/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private string DirName = "Directory";
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var directory = Path.Combine(Directory.GetCurrentDirectory(),DirName);
+            var infor = Directory.GetFiles(directory);
+            return Ok(infor);
+        }
+
+        
+        [HttpPost]
+        public IActionResult Post(IFormFile file)
+        {
+            IActionResult result;
+
+            try
+            {
+                var directory = Path.Combine(Directory.GetCurrentDirectory(), DirName);
+                var dir = Directory.Exists(directory);
+                if (!dir)
+                {
+                    Directory.CreateDirectory(DirName);
+                }
+                var path = Path.Combine(directory, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+               result = Ok("Copied");
+            }catch(Exception e)
+            {
+                result = BadRequest(e.Message); 
+            }
+
+            return result;
+        }
+    }
+}
